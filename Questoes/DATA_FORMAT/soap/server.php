c84877232d79434065bb6edb2c7cb90dc273ecfe
hachicom <adinan_balves@yahoo.com.br>
2016-07-04 00:07:48 -0300

    exercicios soap & rest

---
+++ Questoes/DATA_FORMAT/soap/server.php
@@ -0,0 +1,25 @@
+﻿<?php
+/*
+ * PHP SOAP - How to create a SOAP Server and a SOAP Client
+ */
+
+//a basic API class
+class MyAPI {
+    function hello() {
+        return "Hello, SOAP World!<br>";
+    }
+
+    public function addNumbers($num1,$num2)
+    {
+      return $num1+$num2;
+    }
+}
+//when in non-wsdl mode the uri option must be specified
+$options=array('uri'=>'http://localhost/soap');
+//create a new SOAP server
+$server = new SoapServer(NULL,$options);
+//attach the API class to the SOAP Server
+$server->setClass('MyAPI');
+//start the SOAP requests handler
+$server->handle();
+?>
---
+++ Questoes/DATA_FORMAT/soap/soap_ex1.php
@@ -0,0 +1,37 @@
+﻿ <?php
+ /*
+ * SOAP Exercício 1
+ */
+$options = array('location' => 'http://localhost/GitHub/php_certification/Questoes/DATA_FORMAT/soap/server.php', //digitar errado para testar
+                  'uri' => 'http://localhost/soap',
+                  'exceptions' => 0);
+                  
+// $client = new SoapClient('http://soap.amazon.com/schemas3/AmazonWebServices.wsdl');
+
+// echo "<pre>";
+// var_dump($client->__getFunctions());
+// echo "</pre>";
+
+$client = new SoapClient(NULL, $options);
+echo "<pre>";
+  var_dump($client->__getFunctions()); //Só funciona no WSDL mode
+echo "</pre>";
+
+$result = $client->hello();
+if (is_soap_fault($result)) {
+    //trigger_error("SOAP Fault: (faultcode: {$result->faultcode}, faultstring: {$result->faultstring})", E_USER_ERROR);
+    echo "<pre>Fault message: ".$result->getMessage()."\n";
+    echo "Fault code: ".$result->getCode()."\n";
+    echo "Fault trace: ".var_dump($result->getTrace())."\n";
+    echo "Fault line: ".$result->getLine()."\n";
+    echo "Bad SOAP request\n";
+    echo "REQUEST:\n".$client->__getLastRequest()."\n";
+    echo "RESPONSE:\n".$client->__getLastResponse()."\n";
+    echo "</pre>";
+}else{
+  echo $result;
+}
+
+$result = $client->addNumbers(4,5);
+echo $result;
+ ?>
