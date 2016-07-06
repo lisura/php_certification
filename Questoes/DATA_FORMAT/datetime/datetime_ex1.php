c4a787174adcb418c8d1897faeb5561edb84ee1b
hachicom <adinan_balves@yahoo.com.br>
2016-07-05 23:56:51 -0300

    Exercícios DateTime

---
+++ Questoes/DATA_FORMAT/datetime/datetime_ex1.php
@@ -0,0 +1,13 @@
+﻿<?php
+/* Extraído de PHP.net (date) */
+
+echo '<br>'.date('F j, Y, g:i a');                 
+echo '<br>'.date('m.d.y');                         
+echo '<br>'.date('j, n, Y');                       
+echo '<br>'.date('Ymd');                           
+echo '<br>'.date('h-i-s, j-m-y, it is w Day');     // Aqui vamos ter um valor estranho pois cada caractere está sendo processado
+echo '<br>'.date('\i\t \i\s \t\h\e jS \d\a\y.');   // Aqui os caracteres estão sendo escapados corretamente
+echo '<br>'.date('D M j G:i:s T Y');               
+echo '<br>'.date('H:m:s \m \i\s\ \m\o\n\t\h');     
+echo '<br>'.date('H:i:s');                         
+echo '<br>'.date('Y-m-d H:i:s');                   // Formato de data MySQL
---
+++ Questoes/DATA_FORMAT/datetime/datetime_ex2.php
@@ -0,0 +1,32 @@
+﻿<?php
+/* Exercício 2 */
+echo '<pre>';
+
+$intervalo = new DateInterval('P4M2DT5H4M');
+var_dump($intervalo);
+
+$date = new DateTime('2016-01-11');
+$date->add(new DateInterval('P90D'));
+echo 'Fugi da maldição da leva de janeiro em '.$date->format('d/m/Y') . '<br/>';
+
+$hoje = new DateTime();
+//$hoje = new DateTime("now");
+//$hoje = new DateTimeImmutable("now");
+echo 'Hoje: '.$hoje->format('d/m/Y') . '<br/>';
+
+$amanha = $hoje->add(new DateInterval('P1D'));
+echo 'Amanhã: '.$amanha->format('d/m/Y') . '<br/>';
+
+$depois_de_amanha = $hoje->add(new DateInterval('PT48H'));
+echo 'Depois de Amanhã: '.$depois_de_amanha->format('d/m/Y') . '<br/>';
+
+$semana_que_vem = $hoje->add(new DateInterval('P1W'));
+echo 'Semana que vem: '.$semana_que_vem->format('d/m/Y') . '<br/>';
+
+$semana_que_vem2 = $hoje->add(new DateInterval('P7D'));
+echo 'Semana que vem: '.$semana_que_vem2->format('d/m/Y') . '<br/>';
+
+$deu_ruim = $hoje->add(new DateInterval('P1W4D'));
+echo 'W com D: '.$deu_ruim->format('d/m/Y') . '<br/>';
+
+echo 'Hoje: '.$hoje->format('d/m/Y') . '<br/>';
