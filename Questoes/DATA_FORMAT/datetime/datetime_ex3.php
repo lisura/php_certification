c109f6b61fd1eef4ff462ffa54e23a813d5f6f1b
hachicom <adinan_balves@yahoo.com.br>
2016-07-07 00:29:49 -0300

    Exercícios datetime

---
+++ Questoes/DATA_FORMAT/datetime/datetime_ex3.php
@@ -0,0 +1,30 @@
+﻿<?php
+/* Exercício 3 */
+echo '<pre>';
+
+$date = new DateTime();
+//$date->setDate(1990,12,25);
+$date->sub(new DateInterval('P4D'));
+echo 'Fui informado que consegui emprego na Finnet em '.$date->format('d/m/Y') . '<br/><br/>';
+
+$hoje = new DateTime();
+// $hoje = new DateTime("now");
+// $hoje = new DateTimeImmutable("now");
+echo 'Hoje: '.$hoje->format('d/m/Y') . '<br/>';
+
+$ontem = $hoje->sub(new DateInterval('PT24H'));
+echo 'Ontem: '.$ontem->format('d/m/Y') . '<br/>';
+
+$ano_passado = $hoje->sub(new DateInterval('P1Y'));
+echo 'Ano passado: '.$ano_passado->format('d/m/Y') . '<br/>';
+
+$semana_passada = $hoje->modify('-7 days');
+echo 'Semana passada: '.$semana_passada->format('d/m/Y') . '<br/>';
+
+$semana_passada2 = $hoje->modify('-1 week');
+echo 'Semana passada 2: '.$semana_passada2->format('d/m/Y') . '<br/>';
+
+$semana_que_vem = $hoje->modify('+7 days');
+echo 'Semana que vem: '.$semana_que_vem->format('d/m/Y') . '<br/>';
+
+echo 'Hoje: '.$hoje->format('d/m/Y') . '<br/>';
---
+++ Questoes/DATA_FORMAT/datetime/datetime_ex4.php
@@ -0,0 +1,39 @@
+﻿<?php
+/* Exercício 3 */
+echo '<pre>';
+
+$datetime1 = new DateTime('2016-07-07');
+$datetime2 = new DateTime('2015-11-12');
+$interval = $datetime1->diff($datetime2);
+
+var_dump($interval);
+
+echo '<br><br>===TIMEZONE SET===<br>';
+$timezone_date = new DateTime('now',new DateTimeZone('America/Sao_Paulo'));
+echo $timezone_date->format('d/m/Y H:m:s').'<br/>';
+echo $timezone_date->getTimeZone()->getName().'<br/>';
+echo $timezone_date->getTimestamp().'<br/>';
+
+echo '<br><br>===TIMEZONE PADRAO===<br>';
+$timezone_padrao = new DateTime('now');
+echo $timezone_padrao->format('d/m/Y H:m:s').'<br/>';
+echo $timezone_padrao->getTimeZone()->getName().'<br/>';
+echo $timezone_padrao->getTimestamp().'<br/>';
+
+/*
+America/New_York
+America/Argentina/Buenos_Aires
+Asia/Shanghai
+Asia/Tokyo
+Europe/Athens
+Europe/Monaco
+Africa/Porto-Novo
+Africa/Cairo
+Australia/Sydney
+Australia/Melbourne
+*/
+
+echo '<br><br>===CREATE FROM FORMAT===<br>';
+$date = DateTime::createFromFormat('d/m/Y', '07/07/2016');
+echo $date->format('Y-m-d').'<br/>';
+echo $date->getTimestamp().'<br/>';
