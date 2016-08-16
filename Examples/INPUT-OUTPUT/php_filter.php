617cbc6a6316e3e52d49a6e273818f1267009b5e
ivanmal <ivan.xd2004@gmail.com>
2016-08-15 22:10:46 -0300

    Update php_filter.php

--- Examples/INPUT-OUTPUT/php_filter.php
+++ Examples/INPUT-OUTPUT/php_filter.php
@@ -1,11 +1,11 @@
 <?php
 
 /* Definindo nossa classe de filtro */
-class strtoupper_filter extends php_user_filter {
+class minuscula_filter extends php_user_filter {
   function filter($in, $out, &$consumed, $closing)
   {
     while ($bucket = stream_bucket_make_writeable($in)) {
-      $bucket->data = strtoupper($bucket->data);
+      $bucket->data = strtolower($bucket->data);
       $consumed += $bucket->datalen;
       stream_bucket_append($out, $bucket);
     }
@@ -14,13 +14,13 @@ class strtoupper_filter extends php_user_filter {
 }
 
 /* Registrando nosso filtro no PHP */
-stream_filter_register("strtoupper", "strtoupper_filter")
+stream_filter_register("minuscula", "minuscula_filter")
     or die("Falha ao registrar o filtro");
 
 $fp = fopen("exemplo2.txt", "w");
 
 /* Adiciona o filtro registrado a stream recem aberta */
-stream_filter_append($fp, "strtoupper");
+stream_filter_append($fp, "minuscula");
 
 fwrite($fp, "Pichu\n");
 fwrite($fp, "Pikachu\n");
