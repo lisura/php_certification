70bac2e08a489ce523a9c900c241f338ec2a2d9f
hachicom <adinan0803@gmail.com>
2016-09-30 16:06:06 -0300

    Delete index.php

--- Questoes/SECURITY/index.php
+++
@@ -1,5 +0,0 @@
-<html>
-<div style="background-image: url('http://www.blockfort.com/files/1714/3342/5101/Golvellius.png'); width: 320px; height: 240px;">
-<img src="http://localhost/php_certification/Questoes/SECURITY/usuario.php?cpf=24228124577&senha=123&cod_acesso=123" />
-</div>
-</html>
