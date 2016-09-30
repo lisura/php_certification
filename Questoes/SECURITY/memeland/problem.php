2d5900fb721b177b98cefb76cce3f79464e659fe
hachicom <adinan0803@gmail.com>
2016-09-30 16:03:54 -0300

    Create problem.php

---
+++ Questoes/SECURITY/memeland/problem.php
@@ -0,0 +1,5 @@
+<html>
+<div style="background-image: url('http://i0.kym-cdn.com/photos/images/newsfeed/000/096/044/trollface.jpg?1296494117'); width: 640px; height: 480px;">
+<img src="criar.php?nome=trollface&descricao=Problem?&url=http://i0.kym-cdn.com/photos/images/newsfeed/000/096/044/trollface.jpg?1296494117" />
+</div>
+</html>
