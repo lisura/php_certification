419dc47898d3c457a717b34b4415740f84abdf55
Leandro Lisura <leandrolt@gmail.com>
2016-10-05 23:32:04 -0300

    aula 11 exemplo

---
+++ Examples/SECURITY/uploader.php
@@ -0,0 +1,10 @@
+<?php
+$target_path  =  "/var/www/php_certification/Examples/SECURITY/uploads/";
+$target_path  =  $target_path  .  basename($_FILES['uploadedfile']['name']);
+if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+  echo "The file " . basename($_FILES['uploadedfile']['name']) . " has been uploaded";
+} else {
+  echo "There was an error uploading the file, please try again!";
+}
+echo "<pre>";
+print_r($_FILES);
